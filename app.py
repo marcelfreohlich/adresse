b37af023b243fb2681dfc20b3960209f1733fee6
marcelfreohlich <derdia1897@hotmail.com>
2025-01-29 20:41:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 # Designer-Adressen aus Umgebungsvariablen
 DESIGNERS = [
-    {"name": "Michi", "Josef Musser-Straße 11, 2514, Wienersdorf": os.getenv("DESIGNER_1")},
-    {"name": "Pek", "Keutschach 41, 9074, Keutschach": os.getenv("DESIGNER_2")},
-    {"name": "Sebi", "Unteraumühlweg 6a, 5400, Hallein": os.getenv("DESIGNER_3")},
-    {"name": "Mike", "Lisa Scholl Weg 4, 6170, Zirl": os.getenv("DESIGNER_4")},
-    {"name": "Laura", "Seeweg 16, 6212, Maurach": os.getenv("DESIGNER_5")},
-    {"name": "Stephan", "Hernsteiner Straße 42 Haus 8, 2551, Enzesfeld-Lindabrunn": os.getenv("DESIGNER_6")}
+    {"name": "Michi", "address": os.getenv("DESIGNER_1", "Josef Musser-Straße 11, 2514, Wienersdorf")},
+    {"name": "Pek", "address": os.getenv("DESIGNER_2", "Keutschach 41, 9074, Keutschach")},
+    {"name": "Sebi", "address": os.getenv("DESIGNER_3", "Unteraumühlweg 6a, 5400, Hallein")},
+    {"name": "Mike", "address": os.getenv("DESIGNER_4", "Lisa Scholl Weg 4, 6170, Zirl")},
+    {"name": "Laura", "address": os.getenv("DESIGNER_5", "Seeweg 16, 6212, Maurach")},
+    {"name": "Stephan", "address": os.getenv("DESIGNER_6", "Hernsteiner Straße 42 Haus 8, 2551, Enzesfeld-Lindabrunn")}
 ]
 
 @app.route("/designers", methods=["GET"])
